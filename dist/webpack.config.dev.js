2fcee34fabe88780b94a82032cd0b99a93ef7dc2
xKit-22 <78998441+xKit-22@users.noreply.github.com>
2025-02-12 22:36:06 +0400

    chore: init

---
+++ dist/webpack.config.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.config.common');
+module.exports = merge(commonConfig, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 3001,
+        hot: true,
+        open: true,
+    },
+});
